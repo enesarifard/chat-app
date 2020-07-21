a3698debfef5db4b3805d785bf9a76f35cfa6044
ellie fard <enesarifard@gmail.com>
2020-07-21 09:07:42 -0400

    seccond commit

--- src/index.js
+++ src/index.js
@@ -12,8 +12,17 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+//server (emit) -> client (receive) - countUpdated
+//client (emit) -> server (recieve) - increment
+
+io.on('connection', (socket) => {
   console.log('New WebSocket connection');
+
+  socket.emit('message', 'welcome');
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message);
+  });
 });
 server.listen(port, () => {
   console.log(`Server is running on port ${port} ...`);
