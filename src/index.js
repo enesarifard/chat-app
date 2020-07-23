87776b9b63ddce6ad635a5e4f39487ef0db6644d
ellie fard <enesarifard@gmail.com>
2020-07-23 08:38:28 -0400

    added chatrooms and styles

--- src/index.js
+++ src/index.js
@@ -3,24 +3,29 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const Filter = require('bad-words');
+const { generateMessage, generateLocationMessage } = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-//server (emit) -> client (receive) --acknowledgment-->server
-//client (emit) -> server (recieve) - acknowledgment-->client
-
 io.on('connection', (socket) => {
   console.log('New WebSocket connection');
 
-  socket.emit('message', 'welcome!');
-  socket.broadcast.emit('message', 'A new user has joined');
+  socket.on('join', ({ username, room }) => {
+    socket.join(room);
+
+    socket.emit('message', generateMessage('Welcome!'));
+    socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`));
+
+    // socket.emit, io.emit, socket.broadcast.emit
+    // io.to.emit, socket.broadcast.to.emit
+  });
 
   socket.on('sendMessage', (message, callback) => {
     const filter = new Filter();
@@ -29,20 +34,23 @@ io.on('connection', (socket) => {
       return callback('Profanity is not allowed!');
     }
 
-    io.emit('message', message);
+    io.to('Center City').emit('message', generateMessage(message));
     callback();
   });
 
   socket.on('sendLocation', (coords, callback) => {
-    io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+    io.emit(
+      'locationMessage',
+      generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    );
     callback();
   });
 
   socket.on('disconnect', () => {
-    io.emit('message', 'A user has left');
+    io.emit('message', generateMessage('A user has left!'));
   });
 });
 
 server.listen(port, () => {
-  console.log(`Server is running on port ${port} ...`);
+  console.log(`Server is up on port ${port}!`);
 });
